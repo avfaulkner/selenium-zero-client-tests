7ef70ba4cdd785e27260121fea7bd78432f63ae6
af <afaulkner@teradici.com>
2020-11-02 02:23:44 -0800

    upgrade ubuntu

--- files/parse-logs.py
+++ files/parse-logs.py
@@ -3,23 +3,24 @@ import sys
 import os 
 import io 
  
-input = 'launch-tehama-os.log' 
-output = 'output.txt' 
+input = 'zero-client.log'
+output = 'parsed.txt'
 
 match_list = []
  
 try:
-    with io.open(input, mode = 'r', encoding='utf-16', errors = 'replace') as f:
+    with io.open(input, mode = 'r') as f:
+#     with io.open(input, mode = 'r', encoding='utf-16', errors = 'replace') as f: (if running on WSL)
         lines = f.readlines()
         for line in lines:
-            if line.lower().find('authenticate'.lower()) > 0:
-                print(line)
+            if line.lower().find('web login'.lower()) > 0:
+#                 print(line)
                 match_list.append(line)
 except IOError:
     print("the file you are trying to open doesn't exist.")
 
 with open(output, "w+") as file:
-    file.write("EXPORTED DATA:\n")
+    file.write("Parsed Logs:\n")
     match_list_clean = list(match_list)
     for item in xrange(0, len(match_list_clean)):
         print match_list_clean[item]
